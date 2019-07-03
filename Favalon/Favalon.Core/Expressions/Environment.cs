3a1b7969a449229e5ca13c2dd858aeb1517f8fd6
Kouji Matsui <k@kekyo.net>
2019-07-03 23:51:22 +0900

    wip

--- Favalon/Favalon.Core/Expressions/Environment.cs
+++ Favalon/Favalon.Core/Expressions/Environment.cs
@@ -7,8 +7,24 @@ namespace Favalon.Expressions
 {
     public sealed class Environment
     {
+        private sealed class PlaceholderController
+        {
+            private int index;
+
+            public PlaceholderController()
+            { }
+
+            public PlaceholderExpression Create(Expression higherOrder) =>
+                new PlaceholderExpression(index++, higherOrder);
+        }
+
+        private readonly PlaceholderController placehoderController = new PlaceholderController();
+
         private Environment()
         { }
+
+        public PlaceholderExpression CreatePlaceholder(Expression higherOrder) =>
+            placehoderController.Create(higherOrder);
 
         public Expression Infer(Expression expression)
         {

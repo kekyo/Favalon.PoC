fdaa962b764959863779353ee2c0cdb2d69c9119
Kouji Matsui <k@kekyo.net>
2019-07-05 09:00:48 +0900

    Completed Lambda3

--- Favalon/Favalet.Core.Tests/Expressions/LambdaExpressionTest.cs
+++ Favalon/Favalet.Core.Tests/Expressions/LambdaExpressionTest.cs
@@ -64,5 +64,31 @@ namespace Favalet.Expressions
             var inferred = environment.Infer(expression);
             Assert.AreEqual("(a:System.Int32 -> a:System.Int32):(System.Int32 -> System.Int32)", inferred.StrictReadableString);
         }
+
+        [Test]
+        public void Lambda3()
+        {
+            var environment = Environment.Create();
+
+            /*
+            Lambda 3:
+            a:System.Int32 -> a
+            (a:System.Int32 -> a:?):?
+            1:-------------------
+            (a:System.Int32 -> a:?):'1
+            (a:System.Int32 -> a:?):'1                      : Bind(a:System.Int32)
+            (a:System.Int32 -> a:System.Int32):'1           : Lookup(a => System.Int32), Memoize('1 => (System.Int32 -> System.Int32))
+            2:-------------------
+            (a:System.Int32 -> a:System.Int32):(System.Int32 -> System.Int32)           : Update('1 => (System.Int32 -> System.Int32))
+            3:-------------------
+            System.Int32 -> System.Int32
+            */
+
+            var expression = Lambda(Variable("a", Variable("System.Int32")), Variable("a"));
+            Assert.AreEqual("(a:System.Int32 -> a:?):?", expression.StrictReadableString);
+
+            var inferred = environment.Infer(expression);
+            Assert.AreEqual("(a:System.Int32 -> a:System.Int32):(System.Int32 -> System.Int32)", inferred.StrictReadableString);
+        }
     }
 }

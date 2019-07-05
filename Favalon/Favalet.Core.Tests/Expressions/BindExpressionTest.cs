6f538416ac0a99b7e910149625a13bac65b6ff31
Kouji Matsui <k@kekyo.net>
2019-07-05 20:39:02 +0900

    wip

--- Favalon/Favalet.Core.Tests/Expressions/BindExpressionTest.cs
+++ Favalon/Favalet.Core.Tests/Expressions/BindExpressionTest.cs
@@ -67,5 +67,36 @@ namespace Favalet.Expressions
             var inferred = environment.Infer(expression);
             Assert.AreEqual("(a:('2 -> '2) = (b:'2 -> b:'2):('2 -> '2)):('2 -> '2)", inferred.StrictReadableString);
         }
+
+        [Test]
+        public void Bind3()
+        {
+            var environment = Environment.Create();
+
+            /*
+            Bind 3:
+            a = b -> b:System.Int32
+            (a:? = (b:? -> b:System.Int32):?):?
+            1:-------------------
+            (a:? = (b:? -> b:System.Int32):?):'1
+            (a:'1 = (b:? -> b:System.Int32):?):'1
+            (a:'1 = (b:? -> b:System.Int32):'1):'1
+            (a:'1 = (b:'2 -> b:System.Int32):'1):'1      : Bind(b:'2)
+            (a:'1 = (b:'2 -> b:System.Int32):'1):'1      : Lookup(b => '2), Memoize('2 => System.Int32)
+            2:-------------------
+            (a:'1 = (b:System.Int32 -> b:System.Int32):'1):'1      : Update('2 => System.Int32), Memoize('1 => (System.Int32 -> System.Int32))
+            (a:'1 = (b:System.Int32 -> b:System.Int32):(System.Int32 -> System.Int32)):'1      : Update('1 => (System.Int32 -> System.Int32))
+            (a:(System.Int32 -> System.Int32) = (b:System.Int32 -> b:System.Int32):(System.Int32 -> System.Int32)):'1      : Update('1 => (System.Int32 -> System.Int32))
+            (a:(System.Int32 -> System.Int32) = (b:System.Int32 -> b:System.Int32):(System.Int32 -> System.Int32)):(System.Int32 -> System.Int32)      : Update('1 => (System.Int32 -> System.Int32))
+            3:-------------------
+            System.Int32 -> System.Int32
+            */
+
+            var expression = Bind(Bound("a"), Lambda(Bound("b"), Free("b", Implicit("System.Int32"))));
+            Assert.AreEqual("(a:? = (b:? -> b:System.Int32):?):?", expression.StrictReadableString);
+
+            var inferred = environment.Infer(expression);
+            Assert.AreEqual("(a:(System.Int32 -> System.Int32) = (b:System.Int32 -> b:System.Int32):(System.Int32 -> System.Int32)):(System.Int32 -> System.Int32) ", inferred.StrictReadableString);
+        }
     }
 }

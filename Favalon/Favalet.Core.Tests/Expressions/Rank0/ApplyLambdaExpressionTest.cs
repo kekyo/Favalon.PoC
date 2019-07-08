6b3a44af4e84689ddeb929f563a84dce5715b3ab
Kouji Matsui <k@kekyo.net>
2019-07-09 00:05:47 +0900

    Moved rank0 tests

--- Favalon/Favalet.Core.Tests/Expressions/Rank0/ApplyLambdaExpressionTest.cs
+++ Favalon/Favalet.Core.Tests/Expressions/Rank0/ApplyLambdaExpressionTest.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Favalet.Expressions
+namespace Favalet.Expressions.Rank0
 {
     using static StaticFactories;
 

1696007622860558e7a5e330188ebcd2e9f954a9
Kouji Matsui <k@kekyo.net>
2019-07-12 09:34:28 +0900

    wip

--- Favalon/Favalet.Core/Expressions/Specialized/PlaceholderExpression.cs
+++ Favalon/Favalet.Core/Expressions/Specialized/PlaceholderExpression.cs
@@ -1,3 +1,18 @@
+// This is part of Favalon project.
+// Copyright (c) 2019 Kouji Matsui
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 using System;
 
 namespace Favalet.Expressions.Specialized
@@ -31,6 +46,9 @@ namespace Favalet.Expressions.Specialized
 
         protected override Expression VisitResolving(IResolvingEnvironment environment) =>
             (environment.Lookup(this) is Expression lookup) ? lookup : this;
+
+        internal PlaceholderExpression ReplaceHigherOrder(Expression higherOrder) =>
+            new PlaceholderExpression(this.Index, higherOrder);
 
         public override int GetHashCode() =>
             this.Index;

4d603b1700d808eb33a19f4282436e7dad381a42
Kouji Matsui <k@kekyo.net>
2019-12-02 11:27:15 +0900

    Add mini lambda calculus tests.

---
+++ LambdaCalculus/LambdaCalculus.Tests/BooleanTest.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaCalculus
+{
+    [TestFixture]
+    class BooleanTest
+    {
+        [Test]
+        public void BooleanTrue()
+        {
+            var term = new BooleanTerm(true);
+
+            var actual = term.Reduce();
+
+            Assert.AreEqual(true, ((BooleanTerm)actual).Value);
+        }
+
+        [Test]
+        public void BooleanFalse()
+        {
+            var term = new BooleanTerm(false);
+
+            var actual = term.Reduce();
+
+            Assert.AreEqual(false, ((BooleanTerm)actual).Value);
+        }
+
+        [TestCase(true, true, true)]
+        [TestCase(false, false, true)]
+        [TestCase(false, true, false)]
+        [TestCase(false, false, false)]
+        public void BooleanAnd(bool result, bool lhs, bool rhs)
+        {
+            var term =
+                new ApplyTerm(
+                    new ApplyTerm(
+                        new AndTerm(),
+                        new BooleanTerm(lhs)),
+                    new BooleanTerm(rhs));
+
+            var actual = term.Reduce();
+
+            Assert.AreEqual(result, ((BooleanTerm)actual).Value);
+        }
+    }
+}
---
+++ LambdaCalculus/LambdaCalculus/BooleanTerm.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace LambdaCalculus
+{
+    public abstract class Term
+    {
+        public abstract Term Reduce();
+
+        public abstract Term Call(Term argument);
+    }
+
+    public sealed class BooleanTerm : Term
+    {
+        public readonly bool Value;
+
+        public BooleanTerm(bool value) =>
+            this.Value = value;
+
+        public override Term Reduce() =>
+            this;
+
+        public override Term Call(Term argument) =>
+            throw new InvalidOperationException();
+    }
+
+    public sealed class ApplyTerm : Term
+    {
+        public readonly Term Function;
+        public readonly Term Argument;
+
+        public ApplyTerm(Term function, Term argument)
+        {
+            this.Function = function;
+            this.Argument = argument;
+        }
+
+        public override Term Reduce() =>
+            this.Function.Reduce().Call(this.Argument.Reduce());
+
+        public override Term Call(Term argument) =>
+            throw new InvalidOperationException();
+    }
+
+    public sealed class DelegationTerm : Term
+    {
+        private readonly Func<Term, Term> runner;
+
+        public DelegationTerm(Func<Term, Term> runner) =>
+            this.runner = runner;
+
+        public override Term Reduce() =>
+            this;
+
+        public override Term Call(Term argument) =>
+            runner(argument);
+    }
+
+    public sealed class AndTerm : Term
+    {
+        public AndTerm()
+        { }
+
+        public override Term Reduce() =>
+            this;
+
+        public override Term Call(Term argument) =>
+            new DelegationTerm(rhs => new BooleanTerm(
+                ((BooleanTerm)argument.Reduce()).Value && ((BooleanTerm)rhs.Reduce()).Value));
+    }
+}

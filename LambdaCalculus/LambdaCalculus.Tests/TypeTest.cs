810321ec45426ab13d51a3e2558695b2e1aad63d
Kouji Matsui <k@kekyo.net>
2019-12-04 19:49:11 +0900

    Added order1test

---
+++ LambdaCalculus/LambdaCalculus.Tests/TypeTest.cs
@@ -0,0 +1,24 @@
+﻿using LambdaCalculus.Operators;
+using NUnit.Framework;
+using System;
+
+namespace LambdaCalculus
+{
+    [TestFixture]
+    class TypeTest
+    {
+        [TestCase(typeof(int), 123)]
+        [TestCase(typeof(string), "abc")]
+        [TestCase(typeof(bool), false)]
+        public void Order1Test(Type type, object value)
+        {
+            var term =
+                Term.Constant(value);
+
+            var environment = Environment.Create();
+            var actual = environment.Infer(term);
+
+            Assert.AreEqual(Term.Type(type), actual.HigherOrder);
+        }
+    }
+}

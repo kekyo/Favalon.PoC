141abb7aa959a0261c087eed05719f6e9e76eaf2
Kouji Matsui <k@kekyo.net>
2020-04-16 00:14:01 +0900

    WIP

--- Favalon/Favalet.Core/Contexts/InferContext.cs
+++ Favalon/Favalet.Core/Contexts/InferContext.cs
@@ -43,6 +43,11 @@ namespace Favalet.Contexts
     internal sealed class InferContext :
         TypeContext, IInferContext, IFixupContext
     {
+#if DEBUG
+        private int recursiveCount = 0;
+        private int breakIndex = 5;
+#endif
+
         private readonly IRootTypeContext rootContext;
         private readonly Dictionary<int, IExpression> descriptions;
 
@@ -70,48 +75,70 @@ namespace Favalet.Contexts
             IPlaceholderTerm placeholder,
             IExpression from,
             bool isForward,
+            bool overloadable,
             bool isWiden)
         {
             Debug.Assert(
                 from is PlaceholderTerm ||
                 !(from.HigherOrder is UnspecifiedTerm));
 
+            Debug.Assert(placeholder.Index != breakIndex);
+
+            Debug.WriteLine($"UnifyPlaceholder [{recursiveCount}]: Placeholder={placeholder}, From={from}, IsForward={isForward}, Overloadable={overloadable}, IsWiden={isWiden}");
+
             if (this.descriptions.TryGetValue(placeholder.Index, out var lastCombined))
             {
+                Debug.WriteLine($"UnifyPlaceholder [{recursiveCount}-1]: {lastCombined}");
+
                 var result = isForward ?
-                    this.InternalUnify(lastCombined, from, isWiden) :  // forward
-                    this.InternalUnify(from, lastCombined, isWiden);   // backward
+                    this.InternalUnify(lastCombined, from, overloadable, isWiden) :  // forward
+                    this.InternalUnify(from, lastCombined, overloadable, isWiden);   // backward
 
                 if (result is IExpression)
                 {
+                    Debug.WriteLine($"UnifyPlaceholder [{recursiveCount}-2]: {result}");
+
                     this.descriptions[placeholder.Index] = result;
                     return result;
                 }
-                else if (!isWiden)
+                else if (overloadable && !isWiden)
                 {
                     var combinedExpression = OverloadTerm.From(new[] { lastCombined, from });
+
+                    Debug.WriteLine($"UnifyPlaceholder [{recursiveCount}-3]: {combinedExpression}");
+
                     this.descriptions[placeholder.Index] = combinedExpression!;
                     return combinedExpression;
                 }
                 else
                 {
+                    Debug.WriteLine($"UnifyPlaceholder [{recursiveCount}-4]");
+
                     return null;
                 }
             }
             else
             {
+                Debug.WriteLine($"UnifyPlaceholder [{recursiveCount}-5]");
+
                 this.descriptions.Add(placeholder.Index, from);
                 return from;
             }
         }
 
-        private IExpression? InternalUnify(IExpression to, IExpression from, bool isWiden)
+        private IExpression? InternalUnify(
+            IExpression to, IExpression from,
+            bool overloadable, bool isWiden)
         {
+            Debug.WriteLine($"Unify [{++recursiveCount}]: To={to}, From={from}, Overloadable={overloadable}, IsWiden={isWiden}");
+
             // int: int <-- int
             // IComparable: IComparable <-- IComparable
             // _[1]: _[1] <-- _[1]
             if (to.ExactEquals(from))
             {
+                Debug.WriteLine($"Unify [{recursiveCount--}-1]");
+
                 return to;
             }
 
@@ -119,15 +146,24 @@ namespace Favalet.Contexts
             if (to is IFunctionDeclaredExpression(IExpression tp, IExpression tr) &&
                 from is IFunctionDeclaredExpression(IExpression fp, IExpression fr))
             {
-                var pr = this.InternalUnify(fp, tp, isWiden);
-                var rr = this.InternalUnify(tr, fr, isWiden);
+                Debug.WriteLine($"Unify [{recursiveCount}-2]: TP={tp}, TR={tr}, FP={fp}, FR={fr}");
+
+                var pr = this.InternalUnify(fp, tp, false, isWiden);
+
+                Debug.WriteLine($"Unify [{recursiveCount}-3]: PR={pr}");
+
+                var rr = this.InternalUnify(tr, fr, false, isWiden);
 
                 if (pr != null && rr != null)
                 {
+                    Debug.WriteLine($"Unify [{recursiveCount--}-4]: RR={rr}");
+
                     return FunctionDeclaredExpression.From(pr, rr);
                 }
                 else
                 {
+                    Debug.WriteLine($"Unify [{recursiveCount--}-5]: RR={rr}");
+
                     return null;
                 }
             }
@@ -142,12 +178,19 @@ namespace Favalet.Contexts
             if (to is ISumExpression(IExpression[] tss1) &&
                 from is ISumExpression(IExpression[] fss1))
             {
+                Debug.WriteLine($"Unify [{recursiveCount}-6]");
+
                 var unified = fss1.
-                    Select(fs => tss1.Any(ts => this.InternalUnify(ts, fs, isWiden) != null)).
+                    Select(fs => tss1.Any(ts => this.InternalUnify(ts, fs, overloadable, isWiden) != null)).
                     Memoize();
-                return unified.All(w => w) ?
+
+                var result = unified.All(w => w) ?
                     to :
                     null;
+
+                Debug.WriteLine($"Unify [{recursiveCount--}-7]: Unified={unified}, Result={result}");
+
+                return result;
             }
 
             // (int + double): (int + double) <-- int
@@ -157,21 +200,36 @@ namespace Favalet.Contexts
             // (int + _[1]): (int + _[1]) <-- _[2]
             if (to is ISumExpression(IExpression[] tss2))
             {
-                var results = tss2.
-                    Select(ts => this.InternalUnify(ts, from, isWiden)).
+                Debug.WriteLine($"Unify [{recursiveCount}-8]");
+
+                var unified = tss2.
+                    Select(ts => this.InternalUnify(ts, from, overloadable, isWiden)).
                     Where(result => result != null).
                     Memoize();
-                return OverloadTerm.From(results!);
+
+                var result = OverloadTerm.From(unified!);
+
+                Debug.WriteLine($"Unify [{recursiveCount--}-9]: Unified={unified}, Result={result}");
+
+                return result;
             }
 
             // null: int <-- (int + double)
             if (from is ISumExpression(IExpression[] fss2))
             {
-                var results = fss2.
-                    Select(fs => this.InternalUnify(to, fs, isWiden)).
-                    Where(result => result != null).
+                Debug.WriteLine($"Unify [{recursiveCount}-10]");
+
+                var unified = fss2.
+                    Select(fs => this.InternalUnify(to, fs, overloadable, isWiden)).
                     Memoize();
-                return OverloadTerm.From(results!);
+
+                var result = unified.All(r => r != null) ?
+                    OverloadTerm.From(unified!) :
+                    null;
+
+                Debug.WriteLine($"Unify [{recursiveCount--}-11]: Unified={unified}, Result={result}");
+
+                return result;
             }
 
             // object: object <-- int
@@ -180,29 +238,47 @@ namespace Favalet.Contexts
             if (to is ITypeTerm toType &&
                 from is ITypeTerm fromType)
             {
-                return toType.IsConvertibleFrom(fromType) ?
+                Debug.WriteLine($"Unify [{recursiveCount}-12]: ToType={toType}, FromType={fromType}");
+
+                var result = toType.IsConvertibleFrom(fromType) ?
                     to :
                     null;
+
+                Debug.WriteLine($"Unify [{recursiveCount--}-13]: Result={result}");
+
+                return result;
             }
 
             if (to is IPlaceholderTerm tph)
             {
-                return this.InternalUnifyPlaceholder(tph, from, true, isWiden);
+                var result = this.InternalUnifyPlaceholder(tph, from, true, overloadable, isWiden);
+
+                Debug.WriteLine($"Unify [{recursiveCount--}-14]: Result={result}");
+
+                return result;
             }
             if (from is IPlaceholderTerm fph)
             {
-                return this.InternalUnifyPlaceholder(fph, to, false, isWiden);
+                var result = this.InternalUnifyPlaceholder(fph, to, false, overloadable, isWiden);
+
+                Debug.WriteLine($"Unify [{recursiveCount--}-15]: Result={result}");
+
+                return result;
             }
 
-            return isWiden ?
+            var final = isWiden ?
                 null :
                 OverloadTerm.From(new[] { to, from });
+
+            Debug.WriteLine($"Unify [{recursiveCount--}-16]: Final={final}");
+
+            return final;
         }
 
         public void Unify(IExpression to, IExpression from) =>
-            this.InternalUnify(to, from, false);
+            this.InternalUnify(to, from, true, false);
         public IExpression? Widen(IExpression to, IExpression from) =>
-            this.InternalUnify(to, from, true);
+            this.InternalUnify(to, from, true, true);
 
         public IExpression? Resolve(IPlaceholderTerm placeholder)
         {

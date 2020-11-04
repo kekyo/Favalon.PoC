79c605d96d5cbf6a202ee62647085935482fc61b
Kouji Matsui <k@kekyo.net>
2020-11-04 13:13:32 +0900

    wip

--- Favalon/Favalet.CLR.Core/Parsers/NumericalSignedRunner.cs
+++ Favalon/Favalet.CLR.Core/Parsers/NumericalSignedRunner.cs
@@ -17,39 +17,39 @@ namespace Favalet.Parsers
 
             switch (token)
             {
-                // "-123"
-                case NumericToken numeric:
-                    context.CombineAfter(
-                        ParserUtilities.GetNumericConstant(numeric.Value, context.PreSignToken!.Sign));
-                    context.PreSignToken = null;
-                    return ParseRunnerResult.Empty(
-                        ApplyingRunner.Instance);
-
-                // "- ..."
-                case WhiteSpaceToken _:
-                    // Initial precedence (ArithmericAddition)
-                    context.SetPrecedence(BoundTermPrecedences.ArithmericAddition);
-
-                    // Will make binary op
-                    context.CombineAfter(
-                        new IdentityTerm(context.PreSignToken!.Symbol.ToString()));
-                    context.PreSignToken = null;
-                    return ParseRunnerResult.Empty(
-                        ApplyingRunner.Instance);
-
-                // "-abc"
-                case IdentityToken identity:
-                    // Initial precedence (ArithmericAddition)
-                    context.SetPrecedence(BoundTermPrecedences.ArithmericAddition);
-
-                    // Will make binary op
-                    context.CombineAfter(
-                        new IdentityTerm(context.PreSignToken!.Symbol.ToString()));
-                    context.CombineAfter(
-                        new IdentityTerm(identity.Identity));
-                    context.PreSignToken = null;
-                    return ParseRunnerResult.Empty(
-                        ApplyingRunner.Instance);
+                // // "-123"
+                // case NumericToken numeric:
+                //     context.CombineAfter(
+                //         ParserUtilities.GetNumericConstant(numeric.Value, context.PreSignToken!.Sign));
+                //     context.PreSignToken = null;
+                //     return ParseRunnerResult.Empty(
+                //         ApplyingRunner.Instance);
+                //
+                // // "- ..."
+                // case WhiteSpaceToken _:
+                //     // Initial precedence (ArithmericAddition)
+                //     context.SetPrecedence(BoundTermPrecedences.ArithmericAddition);
+                //
+                //     // Will make binary op
+                //     context.CombineAfter(
+                //         new IdentityTerm(context.PreSignToken!.Symbol.ToString()));
+                //     context.PreSignToken = null;
+                //     return ParseRunnerResult.Empty(
+                //         ApplyingRunner.Instance);
+                //
+                // // "-abc"
+                // case IdentityToken identity:
+                //     // Initial precedence (ArithmericAddition)
+                //     context.SetPrecedence(BoundTermPrecedences.ArithmericAddition);
+                //
+                //     // Will make binary op
+                //     context.CombineAfter(
+                //         new IdentityTerm(context.PreSignToken!.Symbol.ToString()));
+                //     context.CombineAfter(
+                //         new IdentityTerm(identity.Identity));
+                //     context.PreSignToken = null;
+                //     return ParseRunnerResult.Empty(
+                //         ApplyingRunner.Instance);
 
                 default:
                     throw new InvalidOperationException(token.ToString());
